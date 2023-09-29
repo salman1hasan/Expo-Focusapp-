3143d81f9b383c9a1ede9223d45f5f7400bc11a4
Salman Hasan <s.hasan@unguarded.com>
2023-09-28 22:17:15 -0400

    First Commit

---
+++ App.js
@@ -0,0 +1,25 @@
+import { Text, SafeAreaView, StyleSheet } from 'react-native';
+
+// You can import supported modules from npm
+import { Card } from 'react-native-paper';
+
+// or any files within the Snack
+import AssetExample from './components/AssetExample';
+
+export default function App() {
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.paragraph}>
+        Change code in the editor and watch it change on your phone! Save to get a shareable url.
+      </Text>
+      <Card>
+        <AssetExample />
+      </Card>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+ 
+ 
+});
