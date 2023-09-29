8a031ed8b799c8bf8a655bc6cd26da85ef8a59fc
Salman Zuberi Hussain[Sal Hasan] <96196317+salman1hasan@users.noreply.github.com>
2023-09-29 11:25:26 -0400

    Add files via upload

---
+++ unguarded/src/features/Focus.js
@@ -0,0 +1,32 @@
+import React,{useState} from 'react';
+import { View,Text,StyleSheet } from 'react-native'
+import { TextInput } from 'react-native-paper'
+import { colors } from '../utils/colors'
+
+
+export const Focus = () => {
+  const [subject,setSubject] = useState(null)
+  console.log(subject)
+  return (
+      <View style={styles.container}>
+         <View style={styles.inputContainer}>
+          <TextInput onChangeText={setSubject} label="What would you like to focus on"/>
+        </View>
+      </View>
+    );
+};
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex:1,
+  },
+  inputContainer:{
+    flex:0.5,
+    padding:25,
+    justifyContent:'top',
+  },
+  text:{
+    color: colors.white,
+  }
+})
